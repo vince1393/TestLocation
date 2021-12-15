8cd9faceb5dd23bc554839424ea6e1b4d65895b3
Vince Romani <vromani@outlook.com>
2021-12-15 18:56:36 -0500

    Initial Commit

---
+++ TestLocation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestLocation
+//
+//  Created by Vincent Romani on 2021-12-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestLocation/TestLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestLocationApp.swift
+//  TestLocation
+//
+//  Created by Vincent Romani on 2021-12-15.
+//
+
+import SwiftUI
+
+@main
+struct TestLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
